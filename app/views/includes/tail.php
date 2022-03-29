64e0b032af7dadda10269e3b213853e62d1a0672
A-Abd <abrar.abdulwahed@gmail.com>
2022-03-29 23:08:15 +0300

    divide page into partials

---
+++ app/views/includes/tail.php
@@ -0,0 +1,11 @@
+
+        <script src="assets/js/slider.js"></script>
+        <script src="assets/js/cart.js"></script>
+        <script src="assets/js/modals.js"></script>
+        <script src="assets/js/radomAdvertisment.js"></script>
+        <script src="assets/js/search.js"></script>
+        <script src="assets/js/changeLang.js"></script>
+        <script src="assets/js/menuBar.js"></script>
+        <script src="assets/js/counterDown.js"></script>
+    </body>
+</html>
