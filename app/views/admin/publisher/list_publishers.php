a140ebbc45bd5cce519cde95105471c1a79e913e
A-Abd <abrar.abdulwahed@gmail.com>
2022-04-04 05:25:49 +0300

    fix titles of list_publisher page

--- app/views/admin/publisher/list_publishers.php
+++ app/views/admin/publisher/list_publishers.php
@@ -429,7 +429,7 @@
         <div class="content-wrapper">
           <!-- Content -->
           <div class="container-xxl flex-grow-1 container-p-y">
-            <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">الكتب/</span>عرض دور النشر</h4>
+            <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">دور النشر/</span>عرض دور النشر</h4>
             <!-- Multi Column with Form Separator -->
             <!-- Bordered Table -->
             <div class="card">
