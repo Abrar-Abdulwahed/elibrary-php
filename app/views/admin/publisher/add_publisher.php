d413d713f5aab0d1a6f037e993268490202589da
A-Abd <abrar.abdulwahed@gmail.com>
2022-04-04 04:15:04 +0300

    Tuning the fields of adding publisher

--- app/views/admin/publisher/add_publisher.php
+++ app/views/admin/publisher/add_publisher.php
@@ -429,31 +429,49 @@ include_once $URLROOT.'/includes/head.php' ?>
         <div class="content-wrapper">
           <!-- Content -->
           <div class="container-xxl flex-grow-1 container-p-y">     
-            <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">المؤلفين/</span>إدخال مؤلف</h4>
+            <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">دور النشر/</span>ادخال ناشر</h4>
             <!-- Multi Column with Form Separator -->
             <div class="card mb-4">
-              <h5 class="card-header">إدخال مؤلف جديد</h5>
-              <form class="card-body" action="/add_author" method="POST" enctype="multipart/form-data"> 
+              <h5 class="card-header">إدخال دار نشر جديد</h5>
+              <form class="card-body" action="/add_publisher" method="POST" enctype="multipart/form-data"> 
                 <div class="row g-3">
                   <div class="col-md-6">
-                    <label class="form-label" for="multicol-author-name">اسم المؤلف</label>
-                    <input name="author_name" type="text" id="multicol-author-name" class="form-control" placeholder="مثال: أبو حامد الغزالي" />
+                    <label class="form-label" for="multicol-publisher-name">اسم دار النشر</label>
+                    <input name="publisher_name" type="text" id="multicol-publisher-name" class="form-control" placeholder="مثال: أبو حامد الغزالي" />
+                  </div>
+                  <div class="col-md-3">
+                    <label class="form-label" for="multicol-publisher-email">البريد الإلكتروني للناشر</label>
+                    <input name="publisher_email" type="email" id="multicol-publisher-email" class="form-control" placeholder="example@gmail.com" />
+                  </div>
+                  <div class="col-md-3">
+                    <label class="form-label" for="multicol-pic">صورة دار النشر</label>
+                    <div class="input-group input-group-merge">
+                      <input  name="image" type="file"  class="form-control"  aria-describedby="multicol-pic" />    
+                    </div>
+                  </div>
+                  <div class="col-md-3">
+                    <label class="form-label" for="multicol-publisher-phone">الرقم الأول للتواصل مع دار النشر</label>
+                    <input name="publisher_phone" type="text" id="multicol-publisher-phone" class="form-control"  placeholder="(+967)-736565237" />
+                  </div>
+                  <div class="col-md-3">
+                    <label class="form-label" for="multicol-publisher-alt-phone">الرقم الثاني للتواصل مع دار النشر</label>
+                    <input name="publisher_alt_phone" type="text" id="multicol-publisher-alt-phone" class="form-control"  placeholder="(+967)-777777777" />
                   </div>
                   <div class="col-md-6">
-                    <label class="form-label" for="author-bio">نبذة عن المؤلف</label>
-                    <textarea class="form-control" name="author_bio" id="author-bio" cols="3" rows="8"></textarea>
+                    <label class="form-label" for="multicol-publisher-fax">الفاكس</label>
+                    <input name="publisher_fax" type="text" id="multicol-publisher-fax" class="form-control" placeholder="ex. 555-123-4567" />
+                  </div>
+                  <div class="col-md-6">
+                    <label class="form-label" for="multicol-publisher-address">العنوان</label>
+                    <textarea class="form-control" name="publisher_address" id="multicol-publisher-address" cols="3" rows="8"></textarea>
                   </div>
                   <div class="col-md-3">
-                    <label class="form-label" for="multicol-author-email">البريد الإلكتروني للمؤلف</label>
-                    <input name="author_email" type="email" id="multicol-author-email" class="form-control" placeholder="example@gmail.com" />
-                  </div>
-                  <div class="col-md-3">
-                    <label class="form-label" for="multicol-author-phone">رقم التواصل مع المؤلف</label>
-                    <input name="author_phone" type="text" id="multicol-author-phone" class="form-control"  placeholder="(+967)-736565237" />
+                    <label class="form-label" for="multicol-publisher-country">البلد</label>
+                    <input name="publisher_country" type="text" id="multicol-publisher-country" class="form-control" placeholder="اليمن" />
                   </div>
                   <div class="col-md-6">
                     <div class="form-password-toggle">
-                      <label class="form-label" for="multicol-confirm-password">تفعيل المؤلف</label>
+                      <label class="form-label" for="multicol-confirm-password">تفعيل دار النشر</label>
                       <div class="input-group input-group-merge">
                       <label class="switch">
                           <input name="is_active" value=1 type="checkbox" checked class="switch-input" />
